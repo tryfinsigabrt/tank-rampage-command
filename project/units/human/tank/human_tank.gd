c98ff55f52da8842830c8b4c3a6ef269cd949f1a
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-01-19 12:33:14 -0600

    Change tank set up to be character body 3D with single capsule collider and visual mesh hierarchy
    - Add simple character script for testing movement

---
+++ project/units/human/tank/human_tank.gd
@@ -0,0 +1,22 @@
+class_name HumanTank extends CharacterBody3D
+
+@export
+var movement_speed:float = 15.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * movement_speed
+		velocity.z = direction.z * movement_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, movement_speed)
+		velocity.z = move_toward(velocity.z, 0, movement_speed)
+
+	move_and_slide()
