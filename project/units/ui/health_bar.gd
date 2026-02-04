160c20aa8b43f71554778c6d0e375885507ff3f7
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-02-03 21:37:33 -0600

    Adjust max damage and only display health bar if damaged

--- project/units/ui/health_bar.gd
+++ project/units/ui/health_bar.gd
@@ -3,13 +3,30 @@ class_name HealthBar extends Node3D
 @onready var sprite: Sprite3D = $Sprite
 
 @export
+var always_display:bool = false
+
+@export
 var health_stat:HealthStat
 
 func _ready() -> void:
+	_update_visible(health_stat.health_fraction)
+		
 	if not health_stat:
 		push_error("%s: Missing health state set up" % name)
 		return
 	health_stat.health_changed.connect(_on_health_changed.unbind(2))
-	
+
+func _update_visible(fraction:float) -> void:
+	if always_display:
+		return
+		
+	if fraction < 1.0:
+		show()
+	else:
+		hide()
+			
 func _on_health_changed() -> void:
-	sprite.set_instance_shader_parameter(&"health_fraction", health_stat.health_fraction)
+	var fraction:float = health_stat.health_fraction
+	_update_visible(fraction)
+		
+	sprite.set_instance_shader_parameter(&"health_fraction", fraction)
