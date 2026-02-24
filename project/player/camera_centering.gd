9d25b9e904b73105fdd338119d7c00c500717d4c
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-02-23 18:49:34 -0600

    Add logic to reposition player camera to where team's units are

---
+++ project/player/camera_centering.gd
@@ -0,0 +1,57 @@
+class_name CameraCentering extends Node3D
+
+@export
+var ideal_distance_from_closest_focus:float = 100.0
+
+var camera:RTSCamera
+var player_team:MatchTeam
+
+func initialize() -> void:
+	if not camera or not player_team:
+		push_error("%s: camera or player team not set" % name)
+		return
+	SignalBus.match_ready.connect(_on_match_ready.unbind(1), ConnectFlags.CONNECT_ONE_SHOT)
+
+func _on_match_ready() -> void:
+	recenter()
+
+func recenter() -> void:
+	# Get all our units and then determine the average orientation
+	var units:Array[Unit] = player_team.units
+	if not units:
+		print_debug("%s: No units - skipping recentering" % name)
+		return
+	
+	var avg_orientation:Vector3 = Vector3.ZERO
+	for unit in units:
+		avg_orientation += unit.global_forward
+	
+	avg_orientation /= units.size()
+	avg_orientation = avg_orientation.normalized()
+	
+	var bounds:AABB
+	for unit in units:
+		bounds = bounds.expand(unit.global_position)
+	
+	# Find initial camera reference point
+	var center:Vector3 = bounds.get_center()
+	var ray_start:Vector3 = center - avg_orientation * 100000
+	
+	var result:Variant = bounds.intersects_ray(ray_start, avg_orientation)
+	var camera_reference_start:Vector3 
+	
+	if not result:
+		push_warning("%s: Could not find camera start reference - using bounds center with offset")
+		camera_reference_start = center
+	else:
+		camera_reference_start = result as Vector3
+	
+	var camera_position:Vector3 = camera_reference_start - avg_orientation * ideal_distance_from_closest_focus
+	print_debug("%s: Focusing camera on %s" % [name, camera_position])
+	
+	camera.global_position = camera_position
+	# Align rotation along the avg_orientation
+	var forward:Vector3 = -avg_orientation
+	var right := Vector3.UP.cross(forward).normalized()
+	var up := forward.cross(right).normalized()
+	camera.transform.basis = Basis(right, up, forward)
--- project/player/player.gd
+++ project/player/player.gd
@@ -1,5 +1,8 @@
 class_name Player extends Node3D
 
+@onready var camera: RTSCamera = $RTSCamera
+@onready var camera_centering: CameraCentering = $CameraCentering
+
 @export
 var player_team:MatchTeam
 
@@ -11,9 +14,16 @@ func _ready() -> void:
 		return
 		
 	player_unit_actions.team = player_team.team
+	
+	camera_centering.camera = camera
+	camera_centering.player_team = player_team
+	
 	SignalBus.match_ready.connect(_on_match_ready.unbind(1), ConnectFlags.CONNECT_ONE_SHOT)
+	
+	camera_centering.initialize()
 	
 func _on_match_ready() -> void:
 	# Ensure idle action added
 	for unit in player_team.units:
 		unit.get_or_add_actions()
+	
