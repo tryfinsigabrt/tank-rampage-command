f6d04eaf14c5aeab5f3b51fab4b33364d509a641
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-03-06 16:23:43 -0600

    Change calculations to use dist and scale with 1/d^2
    - Also avoids floating point rounding issues

--- project/ai/enemy/flee_avoidance_calculator.gd
+++ project/ai/enemy/flee_avoidance_calculator.gd
@@ -1,6 +1,5 @@
 extends Node
 
-# Anything within 500 units will automatically get a normalized weight of 1.0
 @export_range(0.0, 1e9, 0.01, "or_greater")
 var distance_normalizer:float = 1.0
 
@@ -17,7 +16,7 @@ func _on_avoidance_enemies_changed() -> void:
 	blackboard.explore_heading_bias = heading_dict
 	
 func _calculate_weighted_avoidance_heading(unit:Unit, enemies:Array[Unit]) -> Vector3:
-	var total_dist_sq:float = 0.0
+	var total_dist:float = 0.0
 	var weighted_heading:Vector3 = Vector3.ZERO
 	
 	var unit_pos:Vector3 = unit.global_position
@@ -26,15 +25,14 @@ func _calculate_weighted_avoidance_heading(unit:Unit, enemies:Array[Unit]) -> Ve
 		var enemy_pos:Vector3 = enemy.global_position
 		# To unit so we flee away from the enemy
 		var to_unit:Vector3 = unit_pos - enemy_pos
-		var dist_sq:float = to_unit.length_squared()
+		var dist:float = to_unit.length()
 		
-		# Effectively 1/d^3
-		weighted_heading += to_unit / maxf(0.01, dist_sq * dist_sq)
-		
-		total_dist_sq += dist_sq
+		# Effectively 1/d^2
+		weighted_heading += to_unit / maxf(0.01, dist ** 3)
+		total_dist += dist
 	
 	# Effectively 1/d * N * c
-	weighted_heading *= total_dist_sq * enemies.size() * distance_normalizer
+	weighted_heading *= total_dist * enemies.size() * distance_normalizer
 	#print_debug("%s: Weight Length=%f" % [name, weighted_heading.length()])
 	
 	return weighted_heading
