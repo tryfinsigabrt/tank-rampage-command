65962d649a495cfe610740a622199a749a50b50f
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-03-06 22:16:54 -0600

    Add cluster sphere debug drawing to utility AI debug HUD
    - Troubleshooting why debug draw 3D text doesn't show up with FOW

--- project/ui/debug/debug_hud.gd
+++ project/ui/debug/debug_hud.gd
@@ -3,6 +3,7 @@ extends HFlowContainer
 func _ready() -> void:
 	if not OS.is_debug_build():
 		queue_free()
+		return	
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("toggle_debug_hud"):
--- project/ui/debug/utility_ai_debug_hud_threat_row.gd
+++ project/ui/debug/utility_ai_debug_hud_threat_row.gd
@@ -9,6 +9,10 @@ var team:int
 var _header:String
 var _lines:PackedStringArray
 var _ended:bool
+var _index:int
+
+@export
+var draw_duration:float = 3.0
 
 func _ready() -> void:
 	_header = "TEAM %d\n%s" % [team, SEPARATOR]
@@ -21,6 +25,14 @@ func update(options:Array[UtilityAIOption], chosen_option: UtilityAIOption) -> v
 		_lines.resize(1)
 		_ended = false
 	
+	_index += 1
+	
+	var d_config := DebugDraw3D.new_scoped_config()
+	#d_config.set_text_fixed_size(true)
+	d_config.set_text_outline_size(4)
+	#d_config.set_no_depth_test(true)
+	d_config.set_thickness(0.5)
+
 	# Logic copied from utility_ai.gd as scores not exposed directly
 	var scores: Dictionary[UtilityAIOption, float]
 	for option in options:
@@ -30,18 +42,43 @@ func update(options:Array[UtilityAIOption], chosen_option: UtilityAIOption) -> v
 
 	var context:UnitThreatContext = chosen_option.context
 	# Options already sorted
-	var context_header:String = "CONTEXT: ec=%d fc=%d distance=%.1f estr=%.1f fstr=%.1f" \
-		% [context.threat_size, context.assist_size, context.distance,
+	var context_header:String = "CONTEXT(%d): size=%.1f ec=%d fc=%d distance=%.1f estr=%.1f fstr=%.1f" \
+		% [_index, context.threat_cluster.size, context.threat_size, context.assist_size, context.distance,
 		 context.threat_cluster_strength, context.assist_cluster_strength]
 	_lines.push_back(context_header)
 	_lines.push_back(SEPARATOR)
-
+	
 	for option in options:
 		var entry:String = "%s%s: %.4f" % [" X " if option == chosen_option else "  ", option.action, scores[option]]
 		_lines.push_back(entry)
 		
 	_lines.push_back(SEPARATOR)
-	_label.text = "\n".join(_lines)	
+	_label.text = "\n".join(_lines)
+	
+	# Draw also in world
+	var cluster_center:Vector2 = context.threat_cluster.center
+	var y:float = context.threat_cluster.units.front().global_position.y
+	var center:Vector3 = Vector3(cluster_center.x, y, cluster_center.y)
+	DebugDraw3D.draw_sphere(center, context.threat_cluster.size, _color_from_index(), draw_duration)
+	
+	# Doesn't work with FOW as the FOW quad post-process shader gets drawn over the label
+	DebugDraw3D.draw_text(center + Vector3.UP * 15.0, "%d - %s:%.4f" % [_index, chosen_option.action, scores[chosen_option]], 500, Color.GREEN_YELLOW, draw_duration)
+	#DebugDraw2D.set_text(name + str(_index), "%d - %s:%.4f" % [_index, chosen_option.action, scores[chosen_option]], 0, Color.GREEN_YELLOW, draw_duration)
 
+func _color_from_index() -> Color:
+	match _index - 1:
+		0: return Color.RED
+		1: return Color.GREEN
+		2: return Color.BLUE
+		3: return Color.YELLOW
+		4: return Color.MAGENTA
+		5: return Color.CYAN
+		6: return Color.PURPLE
+		7: return Color.DARK_GREEN
+		8: return Color.GAINSBORO
+		9: return Color.ORANGE_RED
+		_: return Color.BLACK
+		
 func complete() -> void:
 	_ended = true
+	_index = 0
