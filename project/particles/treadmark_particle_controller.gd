000d25a9181bced7b1ab7147322ece2ccaff9eab
Christer Kaitila <breakdance@mcfunkypants.com>
2026-03-10 15:52:09 -0700

    only emit treadmark particles when moving

---
+++ project/particles/treadmark_particle_controller.gd
@@ -0,0 +1,11 @@
+extends CPUParticles3D
+
+# only emit particles if we are moving
+
+var prevPos = Vector3()
+
+func _process(_delta):
+	var dist = prevPos.distance_to(get_parent().position)
+	#print("move dist: "+str(dist))
+	emitting = dist > 0
+	prevPos = get_parent().position
