6111de0f494dae124765c19189673d3b597cb479
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-03-09 19:19:50 -0500

    Initial human marine unit that utilizes marine model and animations
    - Basic implementation for required unit functionality
    - Adds navigation, movement, aiming, shooting
    - Add animation tree to trigger actions via state transitions to play idle, shoot, run, or death animations

---
+++ project/units/human/marine/human_marine_unit.gd
@@ -0,0 +1,123 @@
+class_name HumanMarineUnit extends Unit
+
+@export_range(1.0,360.0, 0.1)
+var turning_speed_degrees:float = 180.0
+
+@onready var health_stat: HealthStat = %HealthStat
+@onready var collision: CollisionShape3D = %Collision
+@onready var visual_root: Node3D = %VisualRoot
+@onready var fire_position: Marker3D = %FirePosition
+@onready var _weapon: Weapon = %Weapon
+@onready var game_unit_navigation: GameUnitNavigation = %GameUnitNavigation
+@onready var animation: MarineAnimation = %MarineAnimation
+
+
+func _is_alive() -> bool:
+	return health_stat.is_alive
+
+func _physics_process(delta: float) -> void:
+	collision.disabled = not is_visible_in_tree()
+
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+# TODO: Some of this can be moved to unit base class or separate component
+func move(input_direction:Vector2, speed_override:float = -1.0) -> void:
+	animation.run()
+	# Move forward/back always proceeds along forward vector 
+	# and left/right rotates in place
+	var input_direction_3:Vector3 = Vector3(input_direction.x, 0, input_direction.y)
+	
+	# Positive rotation is ccw but we want right (+x) to turn model cw so negate
+	var rotation_dir:float = signf(-input_direction.x)
+	var rot:float = deg_to_rad(turning_speed_degrees) * get_physics_process_delta_time() * rotation_dir
+	
+	# Rotate the whole character so that the collider rotates too
+	rotate_y(rot)
+
+	var speed:float = movement_speed if speed_override <= 0.0 else speed_override
+	# Negative as "forward" is -z as we are using right-handed OpenGL-style coordinate system
+	var movement_direction := -input_direction_3.z * visual_root.global_basis.z
+	if movement_direction:
+		velocity.x = movement_direction.x * speed
+		velocity.z = movement_direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
+	
+# TODO: Some of this can be moved to unit base class or separate component
+func aim_at(world_location:Vector3) -> void:
+	# TODO:
+	look_at(world_location)
+	
+	#var aim_direction:Vector3 = (world_location - fire_position.global_position).normalized()
+	#
+	#var heading:Vector3 = get_fire_global_forward()
+	#var projected_forward_vector:Vector2 = Vector2(heading.x, heading.z)
+	#var projected_aim_dir_turret:Vector2 = Vector2(aim_direction.x, aim_direction.z)
+	
+	#Check if we are almost there
+	#var angle:float = rad_to_deg(aim_dir_turret.angle_to(forward_vector))
+	#if absf(angle) > turret_aim_tolerance_deg:
+	#if projected_aim_dir_turret.length() > turret_aim_tolerance:
+		#var rotation_dir:float = -projected_forward_vector.cross(projected_aim_dir_turret)
+		#turret.rotate_turret(rotation_dir)
+	#
+	#var aim_pitch:float = aim_direction.y
+	## Technically this is not an angle but using some small value to avoid jitter
+	#if absf(aim_pitch) > pitch_tolerance:
+		#barrel.pitch_barrel(aim_pitch)
+	
+func shoot() -> void:
+	animation.shoot()
+	_weapon.fire()
+
+func get_fire_global_position() -> Vector3:
+	return fire_position.global_position
+	
+func get_fire_global_forward() -> Vector3:
+	# Positive as we rotated around
+	var orig_basis:Basis = fire_position.global_basis
+	var corrected_basis:Basis = visual_root.transform.basis
+	var final_basis:Basis = orig_basis * corrected_basis
+	return -final_basis.z
+
+func get_fire_global_right() -> Vector3:
+	return fire_position.global_basis.x
+
+func get_fire_global_up() -> Vector3:
+	return fire_position.global_basis.y
+	
+func _is_moving() -> bool:
+	return game_unit_navigation.enabled
+
+func _update_render() -> void:
+	visual_root.visible = render
+
+func _die(damage_params: DamageParameters) -> void:
+	print_debug("%s: Die" % name)
+	animation.die()
+	collision.disabled = true
+	died.emit(damage_params)
+	
+	# Delay so can see visuals
+	# TODO: Get animation notification when complete
+	await get_tree().create_timer(2.0).timeout
+	queue_free()
+	
+func _on_health_changed(previous_health: float, current_health: float) -> void:
+	print_debug("%s: health_changed: %f -> %f" % [name, previous_health, current_health])
+
+func _took_damage(damage_params: DamageParameters) -> void:
+	damaged.emit(damage_params)
+	if health_stat.is_dead:
+		_die(damage_params)
+
+func _get_health_stat() -> HealthStat:
+	return health_stat
+
+func _get_weapon() -> Weapon:
+	return _weapon
---
+++ project/units/human/marine/marine_anim_tree.gd
@@ -0,0 +1,50 @@
+class_name MarineAnimation extends Node3D
+
+enum State
+{
+	IDLE,
+	RUN,
+	SHOOT,
+	DEAD
+}
+
+@export var anim_state_mappings:Dictionary[StringName, State] = {}
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var state_machine:AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
+
+
+var _state:State = State.IDLE
+var _completed_first_transition:bool
+
+var state:State:
+	get: return _state
+	set(value):
+		_state = value
+		
+func _ready() -> void:
+	state_machine.state_started.connect(_on_state_started)
+	state_machine.state_finished.connect(_on_state_finished)
+	
+func idle() -> void:
+	_state = State.IDLE
+
+func run() -> void:
+	_state = State.RUN
+	
+# TODO: Should be able to shoot while running?
+func shoot() -> void:
+	_state = State.SHOOT
+	
+func die() -> void:
+	_state = State.DEAD
+	
+func _on_state_started(state_name: StringName) -> void:
+	if _completed_first_transition:
+		if state_name in anim_state_mappings:
+			var new_state:State = anim_state_mappings[state_name]
+			state = new_state
+
+func _on_state_finished(state_name: StringName) -> void:
+	if state_name in anim_state_mappings:
+		_completed_first_transition = true
