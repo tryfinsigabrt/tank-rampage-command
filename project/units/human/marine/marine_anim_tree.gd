4f960e9d5414de6d0ba9370cacb674f907230d97
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-03-15 12:40:38 -0500

    Add a state update timer to marine animation to avoid unnecessary logic every frame
    - The state transition logic is called every frame for the active state. The derived bools can sometimes recalculate the same thing multiple times in frame
    - Animation state var updates can happen at a lower rate (currently 20fps) and be cached for property accessors
    - In future, having signals drive the variable changes will be more efficient but until/if the animation tree becomes a bottleneck we can do this simpler strategy

--- project/units/human/marine/marine_anim_tree.gd
+++ project/units/human/marine/marine_anim_tree.gd
@@ -15,11 +15,15 @@ var idle_animation_velocity_threshold:float = 0.001
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var state_machine:AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
+@onready var state_tick: Timer = $StateTick
 
 var _unit:HumanMarineUnit
+var _horizontal_speed_sq:float
 
 func _ready() -> void:
 	_unit = Groups.get_parent_in_group(self, Groups.Unit)
+	if animation_tree.active:
+		state_tick.start()
 
 var dead:bool:
 	get:
@@ -29,10 +33,7 @@ var running:bool:
 	get:
 		if shooting:
 			return false
-			
-		var velocity := _unit.velocity
-		var horizontal_speed_sq := Vector2(velocity.x, velocity.z).length_squared()
-		return horizontal_speed_sq >= idle_animation_velocity_threshold
+		return _horizontal_speed_sq >= idle_animation_velocity_threshold
 
 var shooting:bool:
 	get:
@@ -42,10 +43,16 @@ var idle:bool:
 	get:
 		return not running and not shooting
 
-
 func _on_screen() -> void:
 	animation_tree.active = true
-
+	state_tick.start()
 
 func _off_screen() -> void:
 	animation_tree.active = false
+	state_tick.stop()
+
+## Add any per frame calculations here
+## Signal-driven updates are still the most efficient way but avoids re-calculating everything every frame
+func _on_state_tick_timeout() -> void:
+	var velocity := _unit.velocity
+	_horizontal_speed_sq = Vector2(velocity.x, velocity.z).length_squared()
