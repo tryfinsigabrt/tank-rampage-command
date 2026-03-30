759f6046379d977c11fba9261882ce884ef1d4a1
Chris DeLeon <chris@gamkedo.com>
2026-03-30 10:58:56 -0400

    WIP adding lingering dark smoke to artillery vfx

--- project/particles/emit_then_self_remove.gd
+++ project/particles/emit_then_self_remove.gd
@@ -1,8 +1,10 @@
 extends Node
-@onready var particles = $GPUParticles3D
 @export var remove_delay: float = 2.0
 
-func _ready():
-    particles.emitting = true
-    await get_tree().create_timer(remove_delay).timeout
-    queue_free()
+func _ready() -> void:
+	for child in get_children():
+		if child is GPUParticles3D:
+			child.emitting = true
+
+	await get_tree().create_timer(remove_delay).timeout
+	queue_free()
