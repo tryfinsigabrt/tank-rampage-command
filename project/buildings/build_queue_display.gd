ef8a47deecbb9d46f4370c0f2a838a4bb9ef2364
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-04-02 15:38:30 -0500

    Simplify and fix build queue display logic, add additional test scenes, and add to buildings

--- project/buildings/build_queue_display.gd
+++ project/buildings/build_queue_display.gd
@@ -3,7 +3,7 @@ class_name BuildQueue extends Node3D
 @export
 var manufacturing_component:ManufacturingComponent
 
-@onready var sprite: Sprite3D = %Sprite1
+@onready var sprite: Sprite3D = %Sprite01
 @onready var sprite_container: Node3D = %SpriteContainer
 @onready var display_tick: Timer = %DisplayTick
 
@@ -12,17 +12,15 @@ var sprite_size:float = 1.0
 
 var _material_by_type:Dictionary[ConstructionResource.Type, ShaderMaterial] = {}
 var _prototype_material:ShaderMaterial
-var _total_build_count:int
-var _active_build_count:int
+var _queue_count:int
 
 var _sprites:Array[Sprite3D]
-var _active_sprite:Sprite3D
 
 var _build_start_time:float = -1.0
 var _active_build_resource:ConstructionResource
 
 func _ready() -> void:
-	_prototype_material = sprite.material_overlay as ShaderMaterial
+	_prototype_material = sprite.material_override as ShaderMaterial
 	if not _prototype_material:
 		assert(false, "%s: Material on %s is not a ShaderMaterial" % [name, sprite.name])
 		queue_free()
@@ -37,44 +35,46 @@ func _ready() -> void:
 	manufacturing_component.build_completed.connect(_on_build_completed)
 	
 	sprite.visible = false
+	sprite.pixel_size = sprite_size / sprite.texture.get_width()
 	_sprites = [sprite]
 
 func _on_build_queued(resource:ConstructionResource) -> void:
-	_total_build_count += 1
-	_active_build_count += 1
+	_queue_count += 1
 	_create_sprite(resource)
+	
+	#print("QUEUED: %d" % _queue_count)
 		
 func _on_build_started(resource:ConstructionResource) -> void:
 	_build_start_time = GameManager.game_timer.time_seconds
 	_active_build_resource = resource
 	display_tick.start()
 	
-	# Circular buffer logic
-	var active_index:int = (_total_build_count - _active_build_count) % _sprites.size()
-	_active_sprite = _sprites[active_index]
-	# TODO: Need to shift the position visually
-	
+	#print("STARTED: %d" % _queue_count)
+
 func _on_build_completed(_resource:ConstructionResource, _node:Node3D) -> void:
-	_active_build_count -= 1
+	_queue_count -= 1
 	display_tick.stop()
-	
+		
 	_active_build_resource = null
 	_build_start_time = -1.0
-	_active_sprite.visible = false
 	
-	# TODO: Need to shift the positions of the remaining visible ones
+	# Shift over all materials
+	for i in range(1, _queue_count + 1):
+		_sprites[i - 1].material_override = _sprites[i].material_override
+		
+	_sprites[_queue_count].visible = false
 	
-	if _active_build_count == 0:
-		_total_build_count = 0
+	#print("COMPLETED: %d" % _queue_count)
 
 func _create_sprite(resource:ConstructionResource) -> void:
 	var new_sprite:Sprite3D
-	if _active_build_count <= _sprites.size():
-		new_sprite = _sprites[_total_build_count % _sprites.size()]
+	if _queue_count <= _sprites.size():
+		new_sprite = _sprites[_queue_count - 1]
 	else:
 		new_sprite = sprite.duplicate()
+		new_sprite.name = "Sprite%02d" % [_sprites.size() + 1]
 		var pos: Vector3 = _sprites.back().position
-		pos.x += sprite_size
+		pos.x += sprite_size * scale.x
 		new_sprite.position = pos
 		
 		_sprites.push_back(new_sprite)
@@ -85,13 +85,17 @@ func _create_sprite(resource:ConstructionResource) -> void:
 		material = _prototype_material.duplicate()
 		_material_by_type[resource.type] = material
 	
-	new_sprite.material = material
+	new_sprite.material_override = material
 	new_sprite.visible = true
 	material.set_shader_parameter(&"image", resource.icon)
-	material.set_shader_parameter(&"progress", 0.0)
+	
+	# Instance uniforms are set on the geometry instance itself rather than the ShaderMaterial
+	new_sprite.set_instance_shader_parameter(&"progress", 0.0)
 		
 func _tick() -> void:
 	var current_time:float = GameManager.game_timer.time_seconds
 	var progress:float = (current_time - _build_start_time ) / _active_build_resource.time
+	var active_sprite: Sprite3D = _sprites.front()
 	
-	_active_sprite.material_overlay.set_shader_parameter(&"progress", progress)
+	#print("ACTIVE_SPRITE:%s(%d):%f" % [active_sprite.name, _queue_count, progress])
+	active_sprite.set_instance_shader_parameter(&"progress", progress)
