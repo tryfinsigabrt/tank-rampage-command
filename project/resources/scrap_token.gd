271527d572522e23f35bf232669ad5aebf74fc4a
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-04-16 17:38:56 -0500

    Add lifetime to scrap token

--- project/resources/scrap_token.gd
+++ project/resources/scrap_token.gd
@@ -6,6 +6,19 @@ var originating_team:int
 @export_range(1,1e9, 1, "or_greater")
 var scrap:int
 
+## Max lifetime before auto freeing
+@export
+var lifetime:float = 30.0
+
+func _ready() -> void:
+	if lifetime > 0:
+		var timer:Timer = Timer.new()
+		timer.name = "LifetimeTimer"
+		timer.autostart = true
+		timer.one_shot = true
+		timer.wait_time = lifetime
+		timer.timeout.connect(delete)
+		add_child(timer)
 
 func _on_body_entered(body: Node3D) -> void:
 	var unit:Unit = body as Unit
@@ -14,4 +27,7 @@ func _on_body_entered(body: Node3D) -> void:
 	
 	SignalBus.on_scrap_collected.emit(self, unit)
 	
+	delete()
+
+func delete() -> void:
 	queue_free()
