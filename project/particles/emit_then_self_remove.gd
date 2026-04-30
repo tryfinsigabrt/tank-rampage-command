00d6eb1e6c60c17ed6bd8b9ff977947228ecd2fe
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-04-29 20:14:04 -0500

    Scale artillery explosion size with damage radius

--- project/particles/emit_then_self_remove.gd
+++ project/particles/emit_then_self_remove.gd
@@ -1,10 +1,32 @@
-extends Node
-@export var remove_delay: float = 7.0
+extends Node3D
+
+@export 
+var remove_delay: float = 7.0
+
+@export
+var explosion_radius:float = 30.0
+
+@export
+var explosion_size_scale:float = 0.075
+
+@onready var orange_shockwave: GPUParticles3D = $"Orange shockwave"
+@onready var black_lingering_smoke: GPUParticles3D = $"Black lingering smoke"
 
 func _ready() -> void:
+	_set_particle_material_size(orange_shockwave, explosion_radius)
+	_set_particle_material_size(black_lingering_smoke, explosion_radius * 0.5)
+		
 	for child in get_children():
 		if child is GPUParticles3D:
 			child.emitting = true
 
 	await get_tree().create_timer(remove_delay).timeout
 	queue_free()
+
+func _set_particle_material_size(particles: GPUParticles3D, size:float) -> void:
+	var quad_mesh:QuadMesh = particles.draw_pass_1 as QuadMesh
+	if not quad_mesh:
+		return
+	
+	var quad_size := size * explosion_size_scale
+	quad_mesh.size = Vector2(quad_size, quad_size)
--- project/particles/hit/scene_spawning_hit_vfx.gd
+++ project/particles/hit/scene_spawning_hit_vfx.gd
@@ -3,6 +3,8 @@ class_name SceneSpawningHitVfx extends HitVfx
 @export
 var vfx_scene:PackedScene
 
+@export
+var scene_properties:Dictionary[StringName, Variant]
 
 func _ready() -> void:
 	assert(vfx_scene, "Vfx Scene not set!")
@@ -11,7 +13,13 @@ func start(params:DamageParameters) -> void:
 	if not vfx_scene:
 		return
 	
-	var instance:Node = vfx_scene.instantiate()	
+	var instance:Node = vfx_scene.instantiate()
+	
+	# Set extra properties on instance before adding to scene
+	for key in scene_properties:
+		if key in instance:
+			instance.set(key, scene_properties[key])
+			
 	add_child(instance)
 	
 	if instance is Node3D:
