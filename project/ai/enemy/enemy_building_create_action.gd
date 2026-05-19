238768443a8ee2293a0a7707289c24262ae6266a
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-05-18 19:33:46 -0500

    Initial implementation of enemy building creation logic (untested and unused) and initial utility behaviors and considerations

--- project/ai/enemy/enemy_building_create_action.gd
+++ project/ai/enemy/enemy_building_create_action.gd
@@ -18,12 +18,22 @@ var max_spawn_tries_per_frame:int = 10
 @export
 var spawn_timeout:float = 5.0
 
+@export_range(0.1, 1.0, 0.1)
+var radius_expansion_factor:float = 0.5
+
+@export_range(5.0,180.0,1.0)
+var angle_test_increment_deg:float = 45.0
+
 class ActivePlacement:
 	var context:BuildBuildingUtilityContext
 	var spawner:NodePlacementSpawner
-	var resource_bounds:BoundingSphere
+	var resource_bounds:BoundingCircle
 	var start_time:float
 	var latch:Signal
+	var curr_test_radius:float
+	var curr_test_angle:float
+	var curr_bounds_index:int
+	var failed:bool
 	
 	func _init(in_context:BuildBuildingUtilityContext, in_spawner:NodePlacementSpawner) -> void:
 		context = in_context
@@ -33,7 +43,7 @@ class ActivePlacement:
 		latch = Signal(self, "latch")
 		
 		start_time = GameManager.game_timer.time_seconds
-		resource_bounds = Bounds.create_circumscribed_sphere(spawner.asset_aabb)
+		resource_bounds = BoundingCircle.from_aabb(spawner.asset_aabb, true)
 	
 var active_placements:Array[ActivePlacement]
 
@@ -65,8 +75,9 @@ func _process(_delta: float) -> void:
 
 	for active_placement in active_placements:
 		# check for timeout
-		if curr_time - active_placement.start_time >= spawn_timeout:
-			push_warning("%s: Timed out trying to spawn resource %s" % [name, active_placement.construction])
+		if active_placement.failed or curr_time - active_placement.start_time >= spawn_timeout:
+			push_warning("%s: Failed trying to spawn resource %s - reason: %s" % [name, active_placement.construction,
+				"EXHAUSTED" if active_placement.failed else "TIMEOUT"])
 			to_remove.push_back(active_placement)
 			active_placement.latch.emit(null)
 			continue
@@ -88,12 +99,52 @@ func _process(_delta: float) -> void:
 		print_debug("%s: No remaining active placements" % name)
 		set_process(false)
 		
-func _try_spawn(_placement:ActivePlacement) -> Building:
-	# TODO: Use the bounds guidelines in  context along with the bounding sphere to try N locations on arc starting with first center
+func _try_spawn(placement:ActivePlacement) -> Building:
+	# Use the bounds guidelines in  context along with the bounding sphere to try N locations on arc starting with first center
 	# Expand out in a circular pattern in this radius and then move out 0.5x the asset radius and try locations around that arc
 	# Offset the start angle each time up to the delta angle to increase point test coverage and wrap around once hit delta limit
 	# Once exhaust testing one location, then move onto next bounds
 	# It would be better too to add a group tag and place volumes on map to denote ideal spawn locations or mark poor spawn zones to exclude
 	# and then can test those against the proposed locations or ideally never propose them in the first place
 	# We can start with the naive version first
+	var count:int = 0
+	var candidate_bounds: Array[BoundingCircle] = placement.context.target_location_bounds
+	var angle_inc:float = deg_to_rad(angle_test_increment_deg)
+	var resource_bounds:BoundingCircle = placement.resource_bounds
+	var radius_incr:float = resource_bounds.radius * radius_expansion_factor 
+	var spawner:NodePlacementSpawner = placement.spawner
+	
+	for i in range(placement.curr_bounds_index, candidate_bounds.size()):
+		placement.curr_bounds_index = i
+		
+		var curr_bounds:BoundingCircle = candidate_bounds[i]
+		var angle:float = placement.curr_test_angle
+		var center:Vector2 = curr_bounds.center
+		var placement_bounds_radius:float = curr_bounds.radius
+		
+		while placement.curr_test_radius <= placement_bounds_radius:
+			var curr_test_radius:float = placement.curr_test_radius
+			
+			while angle < TAU:
+				var rotated := Vector2.from_angle(angle)
+				var target_grid_pos:Vector2 = rotated * curr_test_radius + center
+				# The grounded checks +- 1000 in y so can just pass zero and let it take care of the height projection
+				var target_pos:Vector3 = Vector3(target_grid_pos.x, 0.0, target_grid_pos.y)
+				spawner.move_to(target_pos)
+				var result:Building = spawner.spawn()
+				if result:
+					return result
+				angle += angle_inc
+				placement.curr_test_angle = angle
+				count += 1
+				# Exhausted time slice
+				if count == max_spawn_tries_per_frame:
+					return null
+			placement.curr_test_angle = 0.0
+			placement.curr_test_radius += radius_incr
+			
+		placement.curr_test_radius = 0.0
+		
+	# Failed - exhausted all positions
+	placement.failed = true
 	return null
--- project/ai/enemy/utility/build_building_utility_context.gd
+++ project/ai/enemy/utility/build_building_utility_context.gd
@@ -3,22 +3,31 @@ class_name BuildBuildingUtilityContext
 var construction:ConstructionResource
 
 var available_scrap:int
-var available_personnel:int
+
+# Based on the available scrap fields for command centers
+var target_location_bounds:Array[BoundingCircle]
+
+var curr_unit_count:int
+
+#region Barracks or Factory
+# Average capacity usage across all buildings of this type
+var avg_queue_depth_fraction:float
+var curr_building_count:int
+
+#endregion
+
+#region Command Center
+# Field with highest depletion fraction
+# 1.0 is fully depleted
+var most_depleted_field_fraction:float
+# Time before all resources across all fields will be depleted
+var time_to_exhaustion:float
+#endregion
 
 var cost:int:
 	get:
 		return construction.cost
 
-var personnel:int:
-	get:
-		return construction.personnel
-
 var remaining_scrap:float:
 	get:
 		return float(available_scrap - cost) / available_scrap if available_scrap > 0 else 0.0
-
-var remaining_personnel:int:
-	get:
-		return available_personnel - personnel
-
-var viable_locations: Array[BoundingSphere]
