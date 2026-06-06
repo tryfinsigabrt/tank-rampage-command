227f6371456bc453957983d83d1a97638d0322d4
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-06-06 15:48:36 -0500

    Add initial bunker defensive structure
    - Export GLB from contributed asset blend file and assign diffuse and normal
    - Set up basic gameplay scene object

---
+++ project/structures/bunker/bunker_structure.gd
@@ -0,0 +1,19 @@
+class_name BunkerStructure extends DefensiveStructure
+
+@onready var visual_root: Node3D = $VisualRoot
+@onready var ui: Node3D = %UI
+
+func _do_update_render(in_visible:bool) -> void:
+	visual_root.visible = in_visible
+	ui.visible = in_visible
+
+func _die(_damage_params: DamageParameters) -> void:
+	print_debug("%s: Die" % name)
+	queue_free()
+	
+func _on_health_changed(previous_health: float, current_health: float) -> void:
+	if LogUtils.verbose:
+		print_debug("%s: health_changed: %f -> %f" % [name, previous_health, current_health])
+
+func _took_damage(_damage_params: DamageParameters) -> void:
+	pass
