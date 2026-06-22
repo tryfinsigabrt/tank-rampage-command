fc19863f93e15ba23fc52c1fc1996e46cad6d16d
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-06-22 18:59:39 -0500

    Enemy units prefer to explore in nearby unit groups

--- project/ai/enemy/explore_surroundings_action.gd
+++ project/ai/enemy/explore_surroundings_action.gd
@@ -10,8 +10,24 @@ var heading_variation_degrees:Vector2 = Vector2(30,120)
 @export
 var world_boundaries_checker:WorldBoundariesChecker
 
+## Prefer exploring as a group up to the max size
+@export
+var max_group_size:int = 10
+
 const MAX_ATTEMPTS:int = 8
+
+@export
+var preferred_unit_group_proximity:float = 100.0
+
+@export
+var min_unit_group_proximity:float = 25.0
+
+var _cluster_creator:ClusterCircleCreator = ClusterCircleCreator.new(preferred_unit_group_proximity)
 	
+const FOLLOWER_UNIT_CLASSES: Array[Unit.UnitClass] = [
+	Unit.UnitClass.Artillery
+]
+
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		return
@@ -23,18 +39,78 @@ func tick(_actor: Node, _blackboard: Blackboard) -> int:
 	var blackboard:EnemyTeamBlackboard = _blackboard
 	
 	var heading_bias_dict: Dictionary[int, Vector3] = blackboard.explore_heading_bias
+		
+	for group in _get_unit_groups(blackboard):
+		var leader:Unit = null
+		for unit:Unit in group:
+			var unit_id:int = unit.get_instance_id()
+			var heading_bias:Vector3 = heading_bias_dict.get(unit_id, Vector3.ZERO)
+			if not leader or heading_bias:
+				var target_pos:Vector3 = _select_move_target(unit, heading_bias)
+				# Only select as leader if not getting a flee vector heading, and we didn't bail out and return a "non-move"
+				if not heading_bias and not target_pos.is_equal_approx(unit.global_position):
+					leader = unit
+			# Provide assistance
+			else:
+				unit.get_or_add_actions().follow(leader)
+						
+	return SUCCESS
 	
-	for unit in blackboard.idle_units:
+func _get_unit_groups(blackboard:EnemyTeamBlackboard) -> Array[Array]:
+	var available_units:Array[Unit] = _get_available_units(blackboard)
+	var groups:Array[Array]
+	if not available_units:
+		return groups
+	
+	_cluster_creator.max_cluster_size = preferred_unit_group_proximity
+	var clusters: Array[ClusterCircle] = _cluster_creator.compute_clusters(available_units)
+	
+	var i:int = 0
+	while i < clusters.size():
+		var cluster:ClusterCircle = clusters[i]
+		# Subdivide
+		while cluster.objects.size() > max_group_size and cluster.size > min_unit_group_proximity:
+			_cluster_creator.max_cluster_size = cluster.size * 0.5
+			var sub_clusters:Array[ClusterCircle] = _cluster_creator.compute_clusters(cluster.objects)
+			cluster = sub_clusters[0]
+			clusters[i] = cluster
+			for j in range(1, sub_clusters.size()):
+				clusters.push_back(sub_clusters[j])
+		var units:Array = cluster.objects
+		# Sort so that artillery units are last and will be followers
+		units.sort_custom(func(a:Unit, b:Unit) -> bool:
+			var a_follower:bool = a.unit_class in FOLLOWER_UNIT_CLASSES
+			var b_follower:bool = b.unit_class in FOLLOWER_UNIT_CLASSES
+			if a_follower != b_follower:
+				# a not a follower so comes first
+				return b_follower
+			return true
+		)
+		groups.push_back(units)
+		i += 1
+		
+	return groups
+	
+func _get_available_units(blackboard:EnemyTeamBlackboard) -> Array[Unit]:
+	var candidate_units := blackboard.idle_units
+	var available_units:Array[Unit]
+	available_units.resize(candidate_units.size())
+	
+	var count:int = 0
+	for i in available_units.size():
+		var unit:Variant = candidate_units[i]
 		# TODO: Shouldn't have to do this - the monitor should be taking care of this but it's not working properly as still 
 		# encounter "previously freed" - another approach would be to convert idle_units to just PackedInt64Array
 		if not is_instance_valid(unit):
 			continue
-		var heading_bias:Vector3 = heading_bias_dict.get(unit.get_instance_id(), Vector3.ZERO)
-		_select_move_target(unit, heading_bias)
-					
-	return SUCCESS
+		
+		available_units[count] = unit
+		count += 1
+	available_units.resize(count)
 	
-func _select_move_target(unit:Unit, heading_bias:Vector3) -> void:
+	return available_units
+	
+func _select_move_target(unit:Unit, heading_bias:Vector3) -> Vector3:
 	if LogUtils.debug:
 		print_debug("%s: Select move target for unit=%s" % [name, unit.name])
 	
@@ -44,8 +120,12 @@ func _select_move_target(unit:Unit, heading_bias:Vector3) -> void:
 		unit.get_or_add_actions().move(target_pos)
 	else:
 		unit.get_or_add_actions().move_and_attack(target_pos)
+	
+	return target_pos
 
 func _get_move_target(unit:Unit, heading_bias_raw:Vector3) -> Vector3:
+	# TODO: Will switch to a region list strategy based on WorldBoundaries that divides up the map into 25x25 region blocks
+	# and marks metadata such as explored, visible to bias toward the frontier and not a random walk from current position
 	var pos:Vector3 = unit.global_position
 	var heading:Vector3 = unit.global_forward
 	
