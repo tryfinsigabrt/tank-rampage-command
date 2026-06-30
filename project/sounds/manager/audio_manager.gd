3ed109577b48a5c1ede6ce0ff70381927b89f26d
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-06-30 12:37:05 -0500

    Remove AudioManagerLegacy since the old code has been completely ported over to the new system

--- project/sounds/manager/audio_manager.gd
+++ project/sounds/manager/audio_manager.gd
@@ -4,19 +4,10 @@ class_name AudioManager extends Node
 @export
 var config: AudioManagerConfig
 
-@onready var legacy_behavior: AudioManagerLegacy = %LegacyBehavior
 @onready var _pools: Node = %Pools
 
 ## Will be removed after AudioManager pool integration is complete
 var _pools_by_key:Dictionary[String, AudioPlayerPool]
-
-#region Legacy Behavior
-## Plays a one-shot sound that is level-bound so should survive it's current parent node attachment
-## Any node can be provided that has a "play" function that takes no arguments
-## This could be an AudioStreamPlayer3D or AudioStreamPlayer
-func play_level_sound(playable_node:Node) -> void:
-	legacy_behavior.play_level_sound(playable_node)
-#endregion
 
 func play_3d(player_config:AudioPlayerConfig, position:Vector3) -> void:
 	if not player_config or not player_config.valid:
