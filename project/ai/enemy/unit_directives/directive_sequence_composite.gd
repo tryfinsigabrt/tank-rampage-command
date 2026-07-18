f154b2a2dd95adb9b8ec900f29fb612f1d9508da
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-07-17 21:32:24 -0500

    Fix AIUnitDirectives behavior tree getting stuck when priorities change because the action leaves weren't reactive composites
    - The current directive action check is supposed to happen every tick but because the sequence node wasn't reactive it never re-evaluated the condition and the tree would remain stuck in the last state

--- project/ai/enemy/unit_directives/directive_sequence_composite.gd
+++ project/ai/enemy/unit_directives/directive_sequence_composite.gd
@@ -1,19 +1,32 @@
 @tool
 class_name DirectiveSequenceComposite extends SequenceComposite
 
+var _running:bool = false
+
 func before_run(actor: Node, blackboard: Blackboard) -> void:
 	super.before_run(actor, blackboard)
 	
 	var directive:AiUnitDirectives = actor
 	directive.started()
+	
+	_running = true
 
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	var result := super.tick(actor,blackboard)
 	
-	if result != RUNNING:
+	if result != RUNNING and _running:
 		var directive:AiUnitDirectives = actor
 		if result == SUCCESS:
 			directive.completed()
 		else:
 			directive.canceled()
+		_running = false
 	return result
+
+func interrupt(actor: Node, blackboard: Blackboard) -> void:
+	if _running:
+		var directive:AiUnitDirectives = actor
+		directive.canceled()
+		_running = false
+		
+	super(actor, blackboard)
