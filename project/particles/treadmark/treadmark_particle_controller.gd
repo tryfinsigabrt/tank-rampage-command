74f6d31920741ad93b44483967c3ffdc14c536f8
Justin Montgomery <tryfinsigabrt@yahoo.com>
2026-07-21 15:03:17 -0500

    Fix treadmark vfx not showing up

--- project/particles/treadmark/treadmark_particle_controller.gd
+++ project/particles/treadmark/treadmark_particle_controller.gd
@@ -1,11 +1,28 @@
 extends CPUParticles3D
 
+@export var heading_offset_degrees:float = 90.0
+
 # only emit particles if we are moving
+var prev_pos:Vector2
 
-var prevPos := Vector3()
+func _ready() -> void:
+	prev_pos = MathUtils.grid_vector(global_position)
+	emitting = false
+	_update_heading()
 
-func _process(_delta: float) -> void:
-	var dist := prevPos.distance_to(get_parent().position)
-	#print("move dist: "+str(dist))
-	emitting = dist > 0
-	prevPos = get_parent().position
+func _tick() -> void:
+	if not is_visible_in_tree():
+		emitting = false
+		return
+		
+	var current_position := MathUtils.grid_vector(global_position)
+	var moving:bool = not current_position.is_equal_approx(prev_pos)
+	
+	if moving:
+		_update_heading()
+		
+	emitting = moving
+	prev_pos = current_position
+
+func _update_heading() -> void:
+	global_rotation.y = get_parent_node_3d().global_rotation.y + deg_to_rad(heading_offset_degrees)
