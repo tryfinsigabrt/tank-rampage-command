bba01aad544ad891f190d02e7cf818697c5d7468
Link of Origin <jesselink333@gmail.com>
2026-07-26 16:28:19 -0600

    Save PlayerSettings to persistent storage

--- project/core/player_settings.gd
+++ project/core/player_settings.gd
@@ -20,6 +20,8 @@ enum ShadowQuality {
 	HIGH,
 }
 
+const SAVE_FILE = "user://tank_rampage_command_settings.save"
+
 var _bus_volumes: Dictionary[StringName, float] = {}
 var _show_fps := false
 var _anti_aliasing: AntiAliasing = AntiAliasing.OFF
@@ -47,6 +49,7 @@ func set_bus_volume(bus_name: StringName, value: float) -> void:
 	_bus_volumes[bus_name] = clamped
 	_apply_bus_volume(bus_name)
 	bus_volume_changed.emit(bus_name, clamped)
+	_save_settings_data()
 
 
 func get_show_fps() -> bool:
@@ -58,6 +61,7 @@ func set_show_fps(value: bool) -> void:
 		return
 	_show_fps = value
 	show_fps_changed.emit(_show_fps)
+	_save_settings_data()
 
 
 func get_anti_aliasing() -> AntiAliasing:
@@ -78,6 +82,7 @@ func set_anti_aliasing(value: AntiAliasing) -> void:
 			viewport.msaa_3d = Viewport.MSAA_2X
 		AntiAliasing.MSAA_4X:
 			viewport.msaa_3d = Viewport.MSAA_4X
+	_save_settings_data()
 
 
 func get_shadow_quality() -> ShadowQuality:
@@ -87,6 +92,7 @@ func get_shadow_quality() -> ShadowQuality:
 func set_shadow_quality(value: ShadowQuality) -> void:
 	_shadow_quality = value
 	shadow_quality_updated.emit(_shadow_quality)
+	_save_settings_data()
 
 func get_anisotropic_filtering() -> Viewport.AnisotropicFiltering:
 	return _anisotropic_filtering
@@ -95,6 +101,7 @@ func set_anisotropic_filtering(value: Viewport.AnisotropicFiltering) -> void:
 	_anisotropic_filtering = value
 	get_viewport().anisotropic_filtering_level = _anisotropic_filtering
 	print("Aniso set to enum idx %s" % get_viewport().anisotropic_filtering_level)
+	_save_settings_data()
 	
 func get_scaling_3d() -> float:
 	return _scaling_3d
@@ -103,6 +110,7 @@ func set_scaling_3d(value: float) -> void:
 	_scaling_3d = clampf(value, 0.2, 4.0)
 	ProjectSettings.set_setting("rendering/scaling_3d/scale", _scaling_3d)
 	get_viewport().scaling_3d_scale = _scaling_3d
+	_save_settings_data()
 
 func get_fsr_enabled() -> bool:
 	return _fsr_is_enabled
@@ -114,6 +122,7 @@ func set_fsr(enabled: bool) -> void:
 		get_viewport().scaling_3d_mode = Viewport.SCALING_3D_MODE_FSR2
 	else:
 		get_viewport().scaling_3d_mode = Viewport.SCALING_3D_MODE_BILINEAR
+	_save_settings_data()
 
 func get_fsr_sharpness() -> float: # 0.0 to 1.0
 	var fsr_sharpness: float = get_viewport().fsr_sharpness
@@ -125,6 +134,7 @@ func set_fsr_sharpness(value: float) -> void:
 	var remapped: float = remap(_fsr_sharpness, 0.0, 1.0, 2.0, 0.0)
 	get_viewport().fsr_sharpness = remapped
 	print("FSR sharpness set to %.2f percent, actual value %.2f" % [_fsr_sharpness, remapped])
+	_save_settings_data()
 
 func get_max_fps() -> int:
 	return _max_fps
@@ -132,6 +142,7 @@ func get_max_fps() -> int:
 func set_max_fps(value: int) -> void:
 	_max_fps = maxi(value, 0)
 	Engine.max_fps = _max_fps
+	_save_settings_data()
 
 func get_ui_scale() -> float:
 	return ProjectSettings.get_setting("display/window/stretch/scale", 1.0)
@@ -140,6 +151,7 @@ func set_ui_scale(value: float) -> void:
 	value = clampf(value, 0.2, 4.0)
 	ProjectSettings.set_setting("display/window/stretch/scale", value)
 	get_tree().root.content_scale_factor = value
+	_save_settings_data()
 
 func _apply_all_bus_volumes() -> void:
 	for bus_name in AUDIO_BUSES:
@@ -155,3 +167,57 @@ func _apply_bus_volume(bus_name: StringName) -> void:
 	var linear := get_bus_volume(bus_name)
 	var volume_db := MUTE_DB if linear <= MIN_LINEAR else linear_to_db(linear)
 	AudioServer.set_bus_volume_db(bus_index, volume_db)
+
+
+func apply_settings(settings: Dictionary) -> void:
+	print("[PlayerSettings] Applying Settings: ", settings)
+	if settings.has("bus_volumes"):
+		_bus_volumes = settings.get("bus_volumes")
+		_apply_all_bus_volumes()
+	if settings.has("show_fps"): set_show_fps(settings.get("show_fps"))
+	if settings.has("fsr_enabled"): set_fsr(settings.get("fsr_enabled"))
+	if settings.has("fsr_sharpness"): set_fsr_sharpness(settings.get("fsr_sharpness"))
+	if settings.has("anti_aliasing"): set_anti_aliasing(settings.get("anti_aliasing"))
+	if settings.has("shadow_quality"): set_shadow_quality(settings.get("shadow_quality"))
+	if settings.has("anisotropic_filtering"): 
+		set_anisotropic_filtering(settings.get("anisotropic_filtering"))
+	if settings.has("scaling_3d"): set_scaling_3d(settings.get("scaling_3d"))
+	if settings.has("max_fps"): set_max_fps(settings.get("max_fps"))
+
+
+func _save_settings_data() -> void:
+	# Overwrites the full file (not append)
+	var save_file := FileAccess.open(SAVE_FILE, FileAccess.WRITE)
+	var settings := _build_settings_dict()
+	var json_settings := JSON.stringify(settings)
+	print("[PlayerSettings] Saving settings data as: ", json_settings)
+	save_file.store_line(json_settings)
+
+
+func _load_settings_data() -> void:
+	if not FileAccess.file_exists(SAVE_FILE):
+		push_warning("No save file detected! (This might be OK!)")
+		return
+	
+	var save_file := FileAccess.open(SAVE_FILE, FileAccess.READ)
+	var json_settings := save_file.get_line()
+	var settings: Dictionary = JSON.parse_string(json_settings)
+	print("[PlayerSettings] Loaded settings: ", settings)
+	if settings != null:
+		apply_settings(settings)
+
+
+func _build_settings_dict() -> Dictionary:
+	var settings := {
+		"bus_volumes": _bus_volumes,
+		"show_fps" : get_show_fps(),
+		"fsr_enabled": get_fsr_enabled(),
+		"fsr_sharpness": get_fsr_sharpness(),
+		"anti_aliasing": get_anti_aliasing(),
+		"shadow_quality": get_shadow_quality(),
+		"anisotropic_filtering": get_anisotropic_filtering(),
+		"scaling_3d": get_scaling_3d(),
+		"max_fps": get_max_fps(),
+		"ui_scale": get_ui_scale(),
+	}
+	return settings
